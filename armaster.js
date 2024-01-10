e49eb97f553e3bc4442098126f983624b3b7d1f1
Fabian Türtscher <ftu5659@students.fhv.at>
2024-01-10 21:56:13 +0100

    finish

--- armaster.js
+++ armaster.js
@@ -1,14 +1,91 @@
+const button = document.getElementById("fullScreenButton");
+const navBar = document.getElementById("navBar");
+const fullScreenElement = document.documentElement;
+const mainElement = document.getElementById("main");
+
+const OUTDOOR = "OUTDOOR";
+const INDOOR = "INDOOR";
+let mode;
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition((position) => {
+    if (position.coords.accuracy < 10 && mode != OUTDOOR) {
+      mode = OUTDOOR;
+      switchMode(mode);
+    } else if (mode != INDOOR) {
+      mode = INDOOR;
+      switchMode(mode);
+    }
+  }, this.showError);
+} else {
+  console.log("Geolocation API is not supported by this browser.");
+}
+
+// Functions to handle Fullscreen
+function openFullscreen() {
+  if (fullScreenElement.requestFullscreen) {
+    fullScreenElement.requestFullscreen();
+  } else if (fullScreenElement.webkitRequestFullscreen) {
+    /* Safari */
+    fullScreenElement.webkitRequestFullscreen();
+  } else if (fullScreenElement.msRequestFullscreen) {
+    /* IE11 */
+    fullScreenElement.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+function fullscreenEnabled() {
+  return !!(
+    document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.msFullscreenElement
+  );
+}
+
+function handleFullscreenChange() {
+  fullscreenEnabled()
+    ? (navBar.style.display = "none")
+    : (navBar.style.display = "block");
+}
+
+if (button) {
+  button.addEventListener("click", () => {
+    fullscreenEnabled() ? closeFullscreen() : openFullscreen();
+  });
+}
+
+document.addEventListener("fullscreenchange", handleFullscreenChange);
+document.addEventListener("mozfullscreenchange", handleFullscreenChange);
+document.addEventListener("webkitfullscreenchange", handleFullscreenChange);
+document.addEventListener("msfullscreenchange", handleFullscreenChange);
+
+// Function to switch between indoors and outdoors
+function switchMode(mode) {
+  const arDoc = mode == INDOOR ? "marker_part.html" : "location_part.html";
+
+  fetch(arDoc)
+    .then((response) => response.text())
+    .then((html) => {
+      mainElement.innerHTML = html;
+      const video = document.querySelector("video");
+
+      video && video.remove();
+    });
+}
+
+// register AFRAME component that logs coordinates for debugging
 AFRAME.registerComponent("log-coordinates", {
   init: function () {
-    this.gpsEl = document.querySelector("[gps-camera]");
-
-    if (this.gpsEl) {
-      this.gpsEl.addEventListener(
-        "gps-camera-update-position",
-        this.onGpsUpdate
-      );
-    }
-
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition((position) => {
         console.log(
@@ -21,17 +98,7 @@ AFRAME.registerComponent("log-coordinates", {
     }
   },
   remove: function () {
-    this.gpsEl.removeEventListener(
-      "gps-camera-update-position",
-      this.onGpsUpdate
-    );
-
     navigator.geolocation.clearWatch();
-  },
-  onGpsUpdate: function (e) {
-    console.log(
-      `ar.js Coordinates\nLatitude: ${e.detail.position.latitude}, Longitude: ${e.detail.position.longitude}`
-    );
   },
   showError: function (error) {
     switch (error.code) {
@@ -51,9 +118,11 @@ AFRAME.registerComponent("log-coordinates", {
   },
 });
 
+// registering an AFRAME component for handling clicks on AR elements
 AFRAME.registerComponent("click-detector", {
   init: function () {
     this.currentMarker = null;
+    this.lock = false;
 
     this.handleMarkerFound = this.handleMarkerFound.bind(this);
     this.handleMarkerLost = this.handleMarkerLost.bind(this);
@@ -62,6 +131,7 @@ AFRAME.registerComponent("click-detector", {
     this.el.addEventListener("markerFound", this.handleMarkerFound);
     this.el.addEventListener("markerLost", this.handleMarkerLost);
     this.el.addEventListener("click", this.handleTap);
+    this.el.addEventListener("touchstart", this.handleTap);
   },
   remove: function () {
     this.el.removeEventListener("markerFound", this.handleMarkerFound);
@@ -70,70 +140,15 @@ AFRAME.registerComponent("click-detector", {
   },
   handleMarkerFound: function (e) {
     this.currentMarker = e.target;
+    console.log("Marker found!!!");
   },
   handleMarkerLost: function () {
     this.currentMarker = null;
   },
-  handleTap: function () {
-    if (this.currentMarker) {
-      const link = this.currentMarker.getAttribute("clickable");
-
-      if (link) {
-        window.open(link, "_blank");
-      }
-    }
+  handleTap: function (e) {
+    const link = this.currentMarker
+      ? this.currentMarker.getAttribute("clickable")
+      : e.target.getAttribute("clickable");
+    link && window.open(link, "_blank");
   },
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("fullScreenButton");
-  const navBar = document.getElementById("navBar");
-  const fullScreenElement = document.documentElement;
-
-  function openFullscreen() {
-    if (fullScreenElement.requestFullscreen) {
-      fullScreenElement.requestFullscreen();
-    } else if (fullScreenElement.webkitRequestFullscreen) {
-      /* Safari */
-      fullScreenElement.webkitRequestFullscreen();
-    } else if (fullScreenElement.msRequestFullscreen) {
-      /* IE11 */
-      fullScreenElement.msRequestFullscreen();
-    }
-  }
-
-  function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  }
-
-  function fullscreenEnabled() {
-    return !!(
-      document.fullscreenElement ||
-      document.webkitFullscreenElement ||
-      document.msFullscreenElement
-    );
-  }
-
-  function handleFullscreenChange() {
-    fullscreenEnabled()
-      ? (navBar.style.display = "none")
-      : (navBar.style.display = "block");
-  }
-
-  if (button) {
-    button.addEventListener("click", () => {
-      fullscreenEnabled() ? closeFullscreen() : openFullscreen();
-    });
-  }
-
-  document.addEventListener("fullscreenchange", handleFullscreenChange);
-  document.addEventListener("mozfullscreenchange", handleFullscreenChange);
-  document.addEventListener("webkitfullscreenchange", handleFullscreenChange);
-  document.addEventListener("msfullscreenchange", handleFullscreenChange);
 });
